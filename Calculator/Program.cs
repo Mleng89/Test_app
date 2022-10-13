dd2bd6f1ca47db62e14ede6563d594659189b3b1
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-10-12 22:46:36 -0400

    First commit test

---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Enter a number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter your operator from (+, -, *, /): ");
+string? op = Console.ReadLine();
+
+Console.Write("Enter another number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+static double calculate(double num1, double num2, string op)
+{
+    //int result;
+    if(op == "+")
+    {
+        return num1 + num2;
+    } else if( op == "-")
+    {
+        return num1 - num2;
+    }
+    else if(op == "*")
+    {
+        return num1 * num2;
+    } else if(op == "/")
+    {
+        return num1 / num2;
+    }
+        return 0.0;
+};
+
+Console.WriteLine(calculate(num1, num2, op));
---
+++ First_App/Program.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * OLD format
+using System;
+
+namespace First_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
+*/
+
+/*
+ * Data types
+ * 
+ * string name = "Matthew";
+ * char grade = 'A';
+ * int age = 33;
+ * 
+ *  float,double,decimal (decimal int)
+ * -less accurate to most accurate ->
+ * 
+ * double gpa = 3.8;
+ * 
+ * bool switchOn = false;
+ * 
+ * 
+ * String templates:
+ * $"Hello, my name is {name}, I am {age} years old."
+ * 
+ * METHODS:
+ * string: 
+ * .Length
+ * .Contains --returns T/F
+ * .IndexOf --does the string contain a certain value? and gives us index
+ * .SubString(index where to start, how many indexes to go) --grab a part of the string
+ * 
+ * Math.
+ * Abs() --absolute value
+ * Pow(3,2) --power of = 9
+ * .Sqrt() --square root
+ */
+
+
+/* 
+ * CREATING METHODS/FUNTION:
+ * 
+static void SayHi(string name)
+{
+    Console.WriteLine($"Hello {name}!");
+    //return $"Hello {name}";
+};
+
+SayHi("Matthew");
+*/
+
+/*
+ * RETURN VALUES 
+static int cube(int num)
+{
+    return num * num * num;
+}
+
+Console.WriteLine(cube(2));
+
+ */
+//for(int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+try
+{
+    Console.Write("Enter a number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter another number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(num1 / num2);
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+Console.ReadLine();
